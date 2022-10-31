2a25546d758fd6b6b8d7f1526422c74721e9ecdf
Hemang <hemanggawali21@gmail.com>
2022-10-31 17:35:30 +0530

    Comments added

--- Personal_Image_Compressor/Image_compressor.py
+++ Personal_Image_Compressor/Image_compressor.py
@@ -19,13 +19,18 @@ label2 = tkinter.Label(
 label3 = tkinter.Label(
     window, text="WOOSH..! Done ", font=("Arial Bold", 26)).pack()
 
+# This will return the the file path, store it in 'file_path'
 file_path = askopenfilename()
+#  Image.open() function to load an image, using a filepath
 img = PIL.Image.open(file_path)
-myHeight, myWidht = img.size
 
-img = img.resize((myHeight, myWidht), PIL.Image.ANTIALIAS)
+myHeight, myWidth = img.size  # storing the original image dimensions
+# Returns a new object/image with the original dimensions, and compressed one
+img = img.resize((myHeight, myWidth), PIL.Image.ANTIALIAS)
 
+# this function is used to save a file in a specific location and store the path in variable 'save_path'
 save_path = asksaveasfilename()
+# saves the compressed image in the 'save_path' dir, with 'compressed.jpg' in the file name
 img.save(save_path + "compressed.jpg")
 
 
